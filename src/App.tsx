11b3836f5c757307e91070ba763d71d974d5ff83
tac3563 <tcox_bu@outlook.com>
2025-05-19 22:57:04 +0100

    [Update] reinstalled project files and start of setting up design system into tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-preset-1">Vite + React</h1>
+      <div className="flex items-center gap-4 p-6"></div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
