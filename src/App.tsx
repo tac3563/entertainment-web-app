9ff5ff0cefe2245df55338f7c370bbb53ad2a622
tac3563 <tom.cox@cite.co.uk>
2025-05-25 22:07:24 +0100

    [Update] set up page routing and nav links

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import PageLayout from "./components/PageLayout.tsx";
 import Home from "./components/Home.tsx";
+import {BrowserRouter} from "react-router-dom";
 
 export default function App() {
-  return (
-    <>
-      <PageLayout>
-        <Home />
-      </PageLayout>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <PageLayout>
+                    <Home/>
+                </PageLayout>
+            </BrowserRouter>
+        </>
+    );
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,7 @@
 import Search from "./Search.tsx";
 import NavBar from "./Navbar.tsx";
 import MediaGrid from "../MediaGrid.tsx";
+import {Routes, Route} from "react-router-dom";
 
 export default function Home() {
     return (
@@ -9,7 +10,20 @@ export default function Home() {
             <main className="max-w-screen-xl w-full">
                 <Search/>
                 <section>
-                    <MediaGrid pageTitle='Home'/>
+                    <Routes>
+                        <Route path='/' element={
+                            <MediaGrid pageTitle='Recommended for you'/>
+                        }/>
+                        <Route path='/movies' element={
+                            <MediaGrid pageTitle='Movies'/>
+                        }/>
+                        <Route path='/tv-shows' element={
+                            <MediaGrid pageTitle='TV shows'/>
+                        }/>
+                        <Route path='/bookmarked' element={
+                            <MediaGrid pageTitle='Bookmarked'/>
+                        }/>
+                    </Routes>
                 </section>
             </main>
         </>
